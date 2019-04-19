c020e4405e5be7f8c99e051eca98868c9c93f9f1
Alexandre Normand <alexandre.normand@gmail.com>
2019-04-18 19:49:24 -0700

    📗 Add godoc Examples in assertanswer and assertplugin

--- test/assertplugin/assertplugin.go
+++ test/assertplugin/assertplugin.go
@@ -8,6 +8,16 @@
 // Users should take special care to use include <@botUserID> with the same botUserID with which the
 // plugin driver has been instantiated in the message text inputs to test commands (or include a
 // channel name that starts with D for direct channel testing)
+//
+// Example:
+//    func TestPlugin(t *testing.T) {
+//        assertplugin := assertplugin.New(t, "bot")
+//        yourPlugin := newPlugin()
+//
+//        assertplugin.AnswersAndReacts(yourPlugin, &slack.Msg{Text: "are you up?"}, func(t *testing.T, answers []*slackscot.Answer, emojis []string) bool {
+// 	          return assert.Len(t, answers, 1) && assertanswer.HasText(t, answers[0], "I'm 😴, you?")
+//        }))
+//    }
 package assertplugin
 
 import (
